87c75f2e78530d656c0ad06b8e9146260fc8ebfc
Thosaengratsami Tassa <ice.izcazy@gmail.com>
2026-01-21 16:33:08 +0700

    as

--- src/practicals/p04/number-pattern.ts
+++ src/practicals/p04/number-pattern.ts
@@ -1,5 +1,18 @@
 // Write your code below
+
 const num = Number(process.argv[2]);
 if(isNaN(num) || num <= 0){
-    process.exit(1);
+    process.exit();
+}
+for(let y = 1; y < num; y--){
+    for(let i =0; i < num ; i++){
+        let line = "";
+        for(let j = 0; j < num - i - 1; j++){
+        line += " ";
+        }
+        for(let k = 0; k < num; k++){
+        line += "";
+        }
+    console.log(line);
+  }
 }
