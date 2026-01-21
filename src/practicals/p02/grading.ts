c98efe8b8608a0c1d55162fa049dc813f5dc3070
Thosaengratsami Tassa <ice.izcazy@gmail.com>
2026-01-21 14:28:22 +0700

    up

--- src/practicals/p02/grading.ts
+++ src/practicals/p02/grading.ts
@@ -1,1 +1,21 @@
 // Write your code below
+const grading = Number(process.argv[2]);
+if(isNaN(grading) || grading < 0 || grading > 100 ){
+    console.log('Invalid input');
+    process.exit(1);
+}
+else if(grading <= 100 && grading >= 80 ){
+    console.log('A');
+}
+else if(grading <= 79 && grading >= 70 ){
+    console.log('B');
+}
+else if(grading <= 69 && grading >= 60 ){
+    console.log('C');
+}
+else if(grading <= 59 && grading >= 50 ){
+    console.log('D');
+}
+else{
+    console.log('F');
+}
