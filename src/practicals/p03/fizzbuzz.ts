ae1dfa2ac8a1c27d53d20915c98927fd5b747d6d
Thosaengratsami Tassa <ice.izcazy@gmail.com>
2026-01-21 16:06:25 +0700

    p03

--- src/practicals/p03/fizzbuzz.ts
+++ src/practicals/p03/fizzbuzz.ts
@@ -1,1 +1,36 @@
 // Write your code below
+const FizBuzz = Number(process.argv[2]);
+if(isNaN(FizBuzz) || FizBuzz <= 0 ){
+    process.exit(1);
+}
+for(let sum = 1; sum <= FizBuzz; sum++){
+    
+    /*if(sum/3 === 1 ){
+        console.log('Fizz');
+    }else if(sum/5 === 1){
+        console.log('Buzz');
+    }
+    else if(sum/3 === 1  sum/5 === 1){
+        console.log('FizzBuzz');
+    }
+    else{
+        console.log(sum);
+    }*/
+    if(sum/3 === 1 && sum/5 === 1){
+        console.log('FizzBuzz');
+    }
+    else if(sum/3 === 1){
+        console.log('Fizz');
+    }
+    else if(sum/5 === 1){
+        console.log('Buzz');
+    }
+    else{
+        console.log(sum);
+    }
+}
+
+
+    
+    
+    
